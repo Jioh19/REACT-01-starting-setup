464c78fa5e17c1f943d1fee3b5f11f1591d6898f
Jioh19 <jioh1982@gmail.com>
2023-08-28 19:44:37 -0400

    add date format

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,16 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
 
 function ExpenseItem(props) {
 
 
 	return (
 		<div className="expense-item">
-			<div>{props.date.toISOString()}</div>
+			<ExpenseDate date={props.expense.date}/>
 			<div className="expense-item__description">
-				<h2>{props.title}</h2>
-				<div className="expense-item__price">${props.amount}</div>
+				<h2>{props.expense.title}</h2>
+				<div className="expense-item__price">${props.expense.amount}</div>
 			</div>
 		</div>
 	);
