bc918eeb4bb937aa676d7d4e40cc19b96033e0e5
Jioh19 <jioh1982@gmail.com>
2023-09-13 22:02:22 -0300

    add component communication

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 	const [enteredTitle, setEnteredTitle] = useState("");
 	const [enteredAmount, setEnteredAmount] = useState("");
 	const [enteredDate, setEnteredDate] = useState("");
@@ -52,13 +52,13 @@ const ExpenseForm = () => {
 		const expenseData = {
 			title: enteredTitle,
 			amount: enteredAmount,
-			date: new     Date(enteredDate),
+			date: new Date(enteredDate),
 		};
 
-		console.log(expenseData);
-		setEnteredTitle('');
-		setEnteredAmount('');
-		setEnteredDate('');
+		props.onSaveExpenseData(expenseData);
+		setEnteredTitle("");
+		setEnteredAmount("");
+		setEnteredDate("");
 	};
 
 	return (
@@ -66,15 +66,28 @@ const ExpenseForm = () => {
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" value={enteredTitle} onChange={titleChangeHandler}></input>
+					<input
+						type="text"
+						value={enteredTitle}
+						onChange={titleChangeHandler}></input>
 				</div>
 				<div className="new-expense__control">
 					<label>mount</label>
-					<input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler}></input>
+					<input
+						type="number"
+						value={enteredAmount}
+						min="0.01"
+						step="0.01"
+						onChange={amountChangeHandler}></input>
 				</div>
 				<div className="new-expense__control">
 					<label>Date</label>
-					<input type="date" value={enteredDate} min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler}></input>
+					<input
+						type="date"
+						value={enteredDate}
+						min="2019-01-01"
+						max="2023-12-31"
+						onChange={dateChangeHandler}></input>
 				</div>
 			</div>
 			<div className="new-expense__actions">
