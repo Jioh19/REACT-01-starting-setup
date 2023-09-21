24a9925d85ab6383dcff1d82963e76eb17a25685
Jioh19 <jioh1982@gmail.com>
2023-09-21 16:23:24 -0300

    fix stateful list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,6 +10,10 @@ function Expenses(props) {
 	const filterChangeHandler = (year) => {
 		setYear(year);
 	};
+
+	const filteredExpenses = props.items.filter((expense) => {
+		return expense.date.getFullYear().toString() === year;
+	});
 	return (
 		<div>
 			<Card className="expenses">
@@ -17,8 +21,9 @@ function Expenses(props) {
 					selected={year}
 					onChangeFilter={filterChangeHandler}
 				/>
-				{props.items.map((expense) => (
+				{filteredExpenses.map((expense) => (
 					<ExpenseItem
+						key={expense.id}
 						title={expense.title}
 						amount={expense.amount}
 						date={expense.date}
