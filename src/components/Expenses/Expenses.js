68f480dc82baa10655f7fae7e8742e2862e48216
Jioh19 <jioh1982@gmail.com>
2023-09-21 18:40:06 -0300

    add expenses list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 
 function Expenses(props) {
@@ -15,18 +16,7 @@ function Expenses(props) {
 		return expense.date.getFullYear().toString() === year;
 	});
 
-	let expensesContent = <p>No expenses found.</p>;
-
-	if (filteredExpenses.length > 0) {
-		expensesContent = filteredExpenses.map((expense) => (
-			<ExpenseItem
-				key={expense.id}
-				title={expense.title}
-				amount={expense.amount}
-				date={expense.date}
-			/>
-		))
-	}
+	
 
 	return (
 		<div>
@@ -35,7 +25,7 @@ function Expenses(props) {
 					selected={year}
 					onChangeFilter={filterChangeHandler}
 				/>
-				{expensesContent}
+				<ExpensesList items={filteredExpenses}/>
 			</Card>
 		</div>
 	);
